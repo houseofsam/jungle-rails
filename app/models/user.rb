4c1e10aaeb4eae47bc882db01a343cbb0fd7f8db
houseofsam <samxsaid@gmail.com>
2022-02-04 05:53:25 -0500

    Prevent user from submitting login/signup form if fields left blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # validation. Ensure an email address is provided
+  # validation. Ensure an email address doesn't exist in DB
   validates :email, uniqueness: true
   has_secure_password
 end
